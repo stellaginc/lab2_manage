044bba161d451161ea0e569697b08257ebcc3574
stellaginc <stellaginc@163.com>
2014-10-13 22:07:03 +0800

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,42 @@
+package action;
+import org.apache.struts2.ServletActionContext; 
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class hello extends ActionSupport{
+	private String userName;
+	private String password;
+	
+	public String execute(){
+		if(getUserName().equals("lab")&&getPassword().equals("123456")){
+			//ActionContext.getContext().getSession().put("name",userName);
+			return "success";
+		}else{
+			return "error";
+		}
+	}
+	
+	/*public void validate(){
+		if(userName==null||userName.equals("")){
+			this.addFieldError("userName","username blank");
+		}
+		if(password==null||password.equals("")){
+			this.addFieldError("password","password blank");
+		}
+	}*/
+	public String getUserName(){
+		return userName;
+	}
+	
+	public void setUserName(String userName){
+		this.userName = userName;
+	}
+	
+	public String getPassword(){
+		return password;
+	}
+	
+	public void setPassword(String password){
+		this.password = password;
+	}
+}
