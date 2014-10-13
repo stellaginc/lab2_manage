258b2542256fb8a14d237a685147b7039b2e14ae
stellaginc <stellaginc@163.com>
2014-10-13 22:51:58 +0800

    change for 1120310114

--- boo.java
+++ boo.java
@@ -7,7 +7,7 @@ public class boo{
 	private String pub;
 	private Date pubdate;
 	private double price;
-	
+	//change for 1120310114
 	public boo(String name,String title,int authorid,String pub,Date pubdate,double price){
 		this.name = name;
 		this.title = title;
@@ -16,49 +16,49 @@ public class boo{
 		this.pubdate = pubdate;
 		this.price = price;
 	}
-	
+
 	public String getName(){
 		return name;
 	}
-	
+
 	public void setName(String name){
 		this.name = name;
 	}
-	
+
 	public String getTitle(){
 		return title;
 	}
-	
+
 	public void setTitle(String title){
 		this.title = title;
 	}
 	public int getAuthorid(){
 		return authorid;
 	}
-	
+
 	public void setAuthorid(int authorid){
 		this.authorid = authorid;
 	}
-	
+
 	public String getPub(){
 		return pub;
 	}
-	
+
 	public void setPub(String pub){
 		this.pub = pub;
 	}
-	
+
 	public Date getPubdate(){
 		return pubdate;
 	}
-	
+
 	public void setPubdate(Date pubdate){
 		this.pubdate = pubdate;
 	}
 	public double getPrice(){
 		return price;
 	}
-	
+
 	public void setPrce(double price){
 		this.price = price;
 	}
